d0ea60634c1b077bbd0be48f3cb5eeb11cc492b2
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2022-01-08 12:44:21 -0300

    Convertion between hours and milliseconds

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -28,4 +28,42 @@ export default class Time {
 
         return `${minutes}:${seconds}.${milliseconds}`;
     }
+
+    public static convertHourToMilliseconds(lapTime: string) :number {
+
+        var lapParts = lapTime.split(".");
+
+        const milliseconds = parseInt(lapParts[1]);
+
+        lapParts = lapParts[0].split(":");
+
+        const hours = parseInt(lapParts[0]);
+        const minutes = parseInt(lapParts[1]);
+        const seconds = parseInt(lapParts[2]);
+
+        return (hours * 3600000) + (minutes * 60000) + (seconds * 1000) + milliseconds;
+    }
+
+    public static convertMillisecondsToHour(lapTime: number) :string {
+
+        let hours = String(Math.floor(lapTime / 3600000));
+
+        let divisionRest = lapTime % 3600000;
+
+        let minutes = String(Math.floor(divisionRest / 60000));
+
+        minutes = minutes.padStart(2, "0");
+
+        divisionRest = lapTime % 60000;
+
+        let seconds = String(Math.floor(divisionRest / 1000));
+
+        seconds = seconds.padStart(2, "0");
+
+        let  milliseconds =  String(divisionRest % 1000);
+
+        milliseconds = milliseconds.padStart(2, "0");
+
+        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    } 
 }
--- test/time.test.ts
+++ test/time.test.ts
@@ -9,3 +9,14 @@ test('convert milliseconds to laptime test', () => {
 
     expect(Time.convertMillisecondsToLapTime(62852)).toBe("1:02.852");
 });
+
+test('convert hour to milliseconds test', () => {
+
+    expect(Time.convertHourToMilliseconds("23:49:08.277")).toBe(85748277);
+});
+
+test('convert milliseconds to hours test', () => {
+
+    expect(Time.convertMillisecondsToHour(85748277)).toBe("23:49:08.277");
+});
+
